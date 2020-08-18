9563043e9c9633eb50c9dbea1ff0fc902d315e8b
Maddy <maddy@madhu.m>
2020-08-18 21:06:51 +0530

    * Http request using axios to reddit API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+function Reddit(){
+    const [posts, setPosts] =  React.useState([]);
+    React.useState(() => {
+            axios.get(`https://www.reddit.com/r/formula1.json`)
+            .then(res => {
+                console.log("res",res);
+                const newPosts = res.data.data.children.map(obj => obj.data)
+
+                setPosts(newPosts)
+            });
+        },[]);
+    return (
+        <div>
+            <h1>https://www.reddit.com/r/formula1</h1>
+            <ul className="posts__lists">
+                {
+                    posts.map(post => (
+                    <li className="post" key={post.id}>
+                        <h4 className="post__title"><a href={post.url}>Title: {post.title} </a> </h4>
+                        <div>Author: <strong>{post.author}</strong> </div>
+                        <div>Scor: <strong>{post.score}</strong></div>
+                    </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Reddit/>,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
